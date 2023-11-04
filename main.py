ddea52035b030362f64dcf829208f960b94e7466
Fulton Browne <fultonbrowne@pm.me>
2023-11-04 13:05:45 -0500

    Made the code way sexier

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ async def gather(ai):
     return ret
 
 
-async def separate_messages(channel, history):
-    # Check if the bot has permissions to read the channel history
+def open_config():
+    with open('config.json', 'r') as config_file:
+        return json.load(config_file)
 
-    # Fetch the last 50 messages from the channel
+
+async def separate_messages(channel, history):
     messages = channel.history(limit=50)
     ml = await gather(messages)
     reversed(ml)
@@ -28,47 +30,34 @@ async def separate_messages(channel, history):
             history.add_user_message(i.content)
 
 
-if __name__ == "__main__":
+config = open_config()
+DISCORD_TOKEN = config.get('token')
+MODEL_NAME = config.get('model')
+ADDRESS = config.get('address')
+llm = Ollama(model="llama2")
+# Define the bot and its intents
+intents = discord.Intents.default()
+intents.messages = True
+intents.dm_messages = True
 
-    with open('config.json', 'r') as config_file:
-        config = json.load(config_file)
-    DISCORD_TOKEN = config.get('token')
-    MODEL_NAME = config.get('model')
-    ADDRESS = config.get('address')
-    llm = Ollama(model="llama2")
-    # Define the bot and its intents
-    intents = discord.Intents.default()
-    intents.messages = True
-    intents.dm_messages = True
-
-    client = discord.Client(intents=intents)
+client = discord.Client(intents=intents)
 
 
-    @client.event
-    async def on_ready():
-        print(f'We have logged in as {client.user}')
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
 
-    @client.event
-    async def on_message(message):
-        print("start message")
+@client.event
+async def on_message(message):
+    if ((message.author != client.user) and
+            (isinstance(message.channel, discord.DMChannel) or client.user in message.mentions)):
         history = ChatMessageHistory()
         await separate_messages(message.channel, history)
         history.add_user_message(message.content)
-        # Ignore messages sent by the bot
-        print(history.messages)
-        if message.author == client.user:
-            return
-
-        # Check if the message is in DMs
-        if isinstance(message.channel, discord.DMChannel) or client.user in message.mentions:
-            async with message.channel.typing():
-                t = llm.predict_messages(messages=history.messages)
-                await message.channel.send(t.content)
-        # elif message.channel.id in allowed_channels:
-        else:
-            return
+        async with message.channel.typing():
+            t = llm.predict_messages(messages=history.messages)
+            await message.channel.send(t.content)
 
 
-    # Replace 'YOUR_BOT_TOKEN' with your actual bot token
-    client.run(DISCORD_TOKEN)
+client.run(DISCORD_TOKEN)
